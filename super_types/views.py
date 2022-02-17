1a62e202cb178a45be50f49af61ff20522929952
Dylyn-Daughtry <98605975+Dylyn-Daughtry@users.noreply.github.com>
2022-02-17 12:12:01 -0500

    Registered supertypes with admin + seeded hero and villain onto table + created supers app

--- super_types/views.py
+++ super_types/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import SuperTypes
+from .serializers import SuperTypesSerializer
 
-# Create your views here.
+# @api_view(['GET'])
+# def super_types_list(request):
+
+
+#     return Response('ok')
--- supers/admin.py
+++ supers/admin.py
@@ -1,4 +1,3 @@
 from django.contrib import admin
-from .models import SuperTypes
+
 # Register your models here.
-admin.site.register(SuperTypes)
