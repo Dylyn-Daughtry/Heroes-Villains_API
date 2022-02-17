1956696fcf5cdf40af71fb4897fa51b0dce8f876
Dylyn-Daughtry <98605975+Dylyn-Daughtry@users.noreply.github.com>
2022-02-17 12:50:37 -0500

    Supers model generated and migrated to databse

--- super_types/models.py
+++ super_types/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class SuperTypes(models.Model):
-    type = models.CharField(max_length=255)
+    type = models.CharField(max_length=255, null=True)
--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+from super_types.models import SuperTypes
+
 # Create your models here.
+class Super(models.Model):
+    name = models.CharField(max_length=255, null=True)
+    alter_ego = models.CharField(max_length=255, null=True)
+    primary_ability = models.CharField(max_length=255, null=True)
+    secondary_ability = models.CharField(max_length=255, null=True)
+    catchphrase = models.CharField(max_length=255, null=True)
+    super_type = models.ForeignKey(SuperTypes, on_delete=models.CASCADE)
